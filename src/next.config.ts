9c30dd2acdc738b419625d346566bb48c9853816
Pham Huu Loc <phamhuulocforwork@gmail.com>
2025-04-27 16:26:38 +0700

    feat: configure image domains in Next.js config

--- src/next.config.ts
+++ src/next.config.ts
@@ -2,7 +2,11 @@ import { NextConfig } from "next";
 
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["github.com", "avatars.githubusercontent.com"],
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
